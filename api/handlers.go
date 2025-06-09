f32ea62149a37fad40b649a8fcf15bf9f548bfdd
David Kleiven <david.kleiven@statnett.no>
2025-06-09 21:52:32 +0200

    feat(api): add api endpoint for proving filtered list of instruments

--- api/handlers.go
+++ api/handlers.go
@@ -1,8 +1,10 @@
 package api
 
 import (
+	"html/template"
 	"net/http"
 
+	"github.com/davidkleiven/caesura/pkg"
 	"github.com/davidkleiven/caesura/web"
 )
 
@@ -10,4 +12,22 @@ func RootHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.Write(web.Index())
+}
+
+func InstrumentSearchHandler(w http.ResponseWriter, r *http.Request) {
+	token := r.URL.Query().Get("token")
+	instruments := pkg.FilterList(allInstruments(), token)
+
+	html := string(web.List())
+	t := template.Must(template.New("list").Parse(html))
+
+	err := t.Execute(w, IdentifiedList{"instruments", instruments})
+	includeError(w, http.StatusInternalServerError, "Failed to render template", err)
+}
+
+func ChoiceHandler(w http.ResponseWriter, r *http.Request) {
+	instrument := r.URL.Query().Get("instrument")
+
+	result := instrument + "\n<input type=\"text\" placeholder=\"Enter part number\" id=\"part-number\"/>"
+	w.Write([]byte(result))
 }
--- api/handlers_test.go
+++ api/handlers_test.go
@@ -24,3 +24,46 @@ func TestRootHandler(t *testing.T) {
 	}
 
 }
+
+func TestInstrumentSearchHandler(t *testing.T) {
+	recorder := httptest.NewRecorder()
+	request := httptest.NewRequest("GET", "/search?token=flute", nil)
+	InstrumentSearchHandler(recorder, request)
+
+	if recorder.Code != 200 {
+		t.Errorf("Expected status code 200, got %d", recorder.Code)
+		return
+	}
+
+	if recorder.Header().Get("Content-Type") != "text/plain; charset=utf-8" {
+		t.Errorf("Expected Content-Type 'text/plain; charset=utf-8', got '%s'", recorder.Header().Get("Content-Type"))
+		return
+	}
+
+	if !strings.Contains(recorder.Body.String(), "Flute") {
+		t.Error("Expected response body to contain 'Flute'")
+		return
+	}
+
+	if strings.Contains(recorder.Body.String(), "Trumpet") {
+		t.Error("Expected response body to not contain 'Trumpet'")
+		return
+	}
+
+}
+
+func TestChoiceHandler(t *testing.T) {
+	recorder := httptest.NewRecorder()
+	request := httptest.NewRequest("GET", "/choice?instrument=flute", nil)
+	ChoiceHandler(recorder, request)
+
+	if recorder.Code != 200 {
+		t.Errorf("Expected status code 200, got %d", recorder.Code)
+		return
+	}
+
+	expectedResponse := "flute\n<input type=\"text\" placeholder=\"Enter part number\" id=\"part-number\"/>"
+	if recorder.Body.String() != expectedResponse {
+		t.Errorf("Expected response body to be '%s', got '%s'", expectedResponse, recorder.Body.String())
+	}
+}
