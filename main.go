d8e46010b834a204502f8f79862b7a19151bc738
David Kleiven <davidkleiven446@gmail.com>
2025-06-19 19:01:10 +0200

    feat: gracefully terminate the application when receiving a sigint and sigterm

--- main.go
+++ main.go
@@ -1,18 +1,44 @@
 package main
 
 import (
-	"log"
+	"context"
 	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/davidkleiven/caesura/api"
 )
 
 func main() {
 	mux := api.Setup()
-
 	port := api.Port()
-	slog.Info("Starting server", "port", port)
-	log.Fatal(http.ListenAndServe(api.Port(), api.LogRequest(mux)))
-	slog.Info("Server stopped")
+
+	server := &http.Server{
+		Addr:    port,
+		Handler: api.LogRequest(mux),
+	}
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		slog.Info("Starting server", "port", port)
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			slog.Error("Server failed", "error", err)
+		}
+	}()
+
+	<-stop
+	slog.Info("Shutting down server")
+	ctx, cancel := context.WithTimeout(context.Background(), 5.0*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("Server shutdown failed", "error", err)
+	} else {
+		slog.Info("Server gracefully stopped")
+	}
 }
