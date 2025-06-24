1b9e378a7e2b6a85938f941a791e12ba222e2837
David Kleiven <davidkleiven446@gmail.com>
2025-06-24 11:28:42 +0200

    feat(web): add assign button

--- web/js/pdf-viewer.js
+++ web/js/pdf-viewer.js
@@ -1,3 +1,4 @@
+pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js';
 const fileInput = document.getElementById('file-input');
 const canvas = document.getElementById('pdf-canvas');
 const ctx = canvas.getContext('2d');
@@ -6,12 +7,15 @@ const pageNumSpan = document.getElementById('page-num');
 const pageCountSpan = document.getElementById('page-count');
 const prevPageBtn = document.getElementById('prev-page');
 const nextPageBtn = document.getElementById('next-page');
+const assignPageBtn = document.getElementById('assign-page');
+const assignmentSection = document.getElementById('assignments');
 
 let pdfDoc = null;
 let currentPage = 1;
 
 const renderPage = (num) => {
     pdfDoc.getPage(num).then((page) => {
+        if (!pdfDoc) return;
         const viewport = page.getViewport({ scale: 1.5 });
         canvas.height = viewport.height;
         canvas.width = viewport.width;
@@ -26,7 +30,8 @@ const renderPage = (num) => {
     };
 
     const queueRenderPage = (num) => {
-    if (num < 1 || num > pdfDoc.numPages) return;
+
+    if (!pdfDoc || num < 1 || num > pdfDoc.numPages) return;
     currentPage = num;
     renderPage(currentPage);
     };
@@ -37,6 +42,11 @@ const renderPage = (num) => {
 
     nextPageBtn.addEventListener('click', () => {
     queueRenderPage(currentPage + 1);
+    })
+
+    assignPageBtn.addEventListener('click', () => {
+        if (addAssignment() != 0) return;
+        queueRenderPage(currentPage + 1);
     })
 
     fileInput.addEventListener('change', (e) => {
@@ -58,3 +68,70 @@ const renderPage = (num) => {
         alert('Please upload a valid PDF file.');
     }
     });
+
+function addAssignment() {
+    const chosenInstrument = document.getElementById('chosen-instrument');
+    const currentPage = pageNumSpan.textContent;
+    const assignmentDesc = chosenInstrument.textContent + document.getElementById('part-number')?.value || "";
+
+    if (!assignmentDesc) {
+        alert('Please select an instrument and enter a part number.');
+        return 1;
+    }
+
+    const assignmentId = assignmentDesc.toLowerCase().replace(/\s+/g, '');
+
+    const assignmentDiv = document.getElementById(assignmentId);
+    if (!assignmentDiv) {
+        const color = assignementColor(assignmentId);
+        assignmentSection.innerHTML += `
+            <div id="${assignmentId}-group" class="relative group inline-block pr-2">
+                <button id=${assignmentId} onclick="this.parentElement.remove()" class="flex text-white ${color} py-2 px-4 rounded-lg">
+                    <span id="${assignmentId}-from">${currentPage}</span> -
+                    <span id="${assignmentId}-to">${currentPage}</span>
+                </button>
+                <div class="absolute bottom-full left-1/2 -translate-x-1/2 mb-2
+                hidden group-hover:block bg-gray-800 text-white text-xs
+                rounded px-2 py-1 z-10 whitespace-nowrap">
+                ${assignmentDesc}
+            </div>
+        `;
+    } else {
+        const currentFrom = parseInt(document.getElementById(`${assignmentId}-from`).textContent)
+        const newTo = parseInt(currentPage);
+        if (newTo >= currentFrom) {
+            document.getElementById(`${assignmentId}-to`).textContent = newTo;
+        } else {
+            alert('Attempting to assign a page before the current first page. If you want to change the first page, please remove the assignment and reassign it.');
+            return 1;
+        }
+    }
+    return 0;
+}
+
+function assignementColor(assignmentId) {
+    console.log("Assignement ID: ", assignmentId);
+    if (assignmentId.toLowerCase().includes("trumpet") || assignmentId.toLowerCase().includes("cornet")) {
+        return "bg-red-400 hover:bg-red-500";
+    } else if (assignmentId.toLowerCase().includes("trombone")) {
+        return "bg-yellow-400 hover:bg-yellow-500";
+    }
+    else if (assignmentId.toLowerCase().includes("tuba") || assignmentId.toLowerCase().includes("bass")) {
+        return "bg-yellow-600 hover:bg-yellow-700";
+    } else if (assignmentId.toLowerCase().includes("clarinet")) {
+        return "bg-green-400 hover:bg-green-500";
+    }
+    else if (assignmentId.toLowerCase().includes("saxophone")) {
+        return "bg-green-600 hover:bg-green-700";
+    }
+    else if (assignmentId.toLowerCase().includes("flute")) {
+        return "bg-purple-600 hover:bg-purple-700";
+    }
+    else if (assignmentId.toLowerCase().includes("oboe")) {
+        return "bg-blue-400 hover:bg-blue-500";
+    }
+    else if (assignmentId.toLowerCase().includes("bassoon")) {
+        return "bg-blue-600 hover:bg-blue-700";
+    }
+    return "bg-gray-400 hover:bg-gray-500";
+}
--- web/templates.go
+++ web/templates.go
@@ -16,3 +16,7 @@ func Index() []byte {
 func List() []byte {
 	return utils.Must(templatesFS.ReadFile("templates/list.html"))
 }
+
+func Assignments() []byte {
+	return utils.Must(templatesFS.ReadFile("templates/assignments.html"))
+}
