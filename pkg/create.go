b03b88468875c7aa2cf50478f95d38967f2cb599
David Kleiven <davidkleiven446@gmail.com>
2025-06-28 10:28:56 +0200

    feat(pkg): make function for creating pdfs available via the pkg module

---
+++ pkg/create.go
@@ -0,0 +1,44 @@
+package pkg
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+
+	"github.com/pdfcpu/pdfcpu/pkg/api"
+	"github.com/pdfcpu/pdfcpu/pkg/pdfcpu/primitives"
+)
+
+func createTextBox(txt string) *primitives.TextBox {
+	return &primitives.TextBox{
+		Value:    txt,
+		Position: [2]float64{100, 100},
+		Font: &primitives.FormFont{
+			Name: "Helvetica",
+			Size: 12,
+		},
+	}
+}
+
+func CreateNPagePdf(w io.Writer, n int) error {
+	pages := make(map[string]*primitives.PDFPage, n)
+	for i := 0; i < n; i++ {
+		pageNumber := i + 1
+		pages[fmt.Sprintf("%d", pageNumber)] = &primitives.PDFPage{
+			Content: &primitives.Content{
+				TextBoxes: []*primitives.TextBox{
+					createTextBox(fmt.Sprintf("This is page %d", pageNumber)),
+				},
+			},
+		}
+	}
+
+	desc := primitives.PDF{Pages: pages}
+
+	data, err := json.Marshal(desc)
+	if err != nil {
+		return err
+	}
+	return api.Create(nil, bytes.NewBuffer(data), w, nil)
+}
