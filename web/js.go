bf90fb35f338cd01694704d84ca2d4aa46b0fd97
David Kleiven <davidkleiven446@gmail.com>
2025-07-03 18:07:06 +0200

    chore: add dependabot step for updating htmx
    Closes #22

--- web/js.go
+++ web/js.go
@@ -2,7 +2,11 @@ package web
 
 import (
 	"embed"
+	"encoding/json"
 	"net/http"
+
+	"github.com/davidkleiven/caesura/pkg"
+	"github.com/davidkleiven/caesura/utils"
 )
 
 //go:embed js/*
@@ -10,4 +14,19 @@ var jsFS embed.FS
 
 func JsServer() http.Handler {
 	return http.FileServer(http.FS(jsFS))
+}
+
+type JsPackages struct {
+	Dependencies `json:"dependencies"`
+}
+
+type Dependencies struct {
+	HtmxVersion string `json:"htmx.org"`
+}
+
+func LoadDependencies() JsPackages {
+	var deps JsPackages
+	data := utils.Must(jsFS.ReadFile("js/package.json"))
+	pkg.PanicOnErr(json.Unmarshal(data, &deps))
+	return deps
 }
