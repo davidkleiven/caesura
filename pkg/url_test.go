75bc78dc99f01f3b1c13bc9f3b74505423baa800
David Kleiven <david.kleiven@statnett.no>
2025-07-09 14:16:19 +0200

    refactor: make function for parsing parametrized URLs

---
+++ pkg/url_test.go
@@ -0,0 +1,34 @@
+package pkg
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestUrl(t *testing.T) {
+	for _, test := range []struct {
+		Url        string
+		ShouldFail bool
+		Expect     InterpretedUrl
+	}{
+		{
+			Url:    "/resource/123",
+			Expect: InterpretedUrl{Path: "resource", PathParameter: "123"},
+		},
+		{
+			Url:        "/missing-resource",
+			ShouldFail: true,
+		},
+	} {
+		t.Run(test.Url, func(t *testing.T) {
+			result, err := ParseUrl(test.Url)
+			if test.ShouldFail && !errors.Is(err, ErrCanNotInterpretUrl) {
+				t.Errorf("Expected %s got %s", ErrCanNotInterpretUrl, err)
+			}
+
+			if !test.ShouldFail && result != test.Expect {
+				t.Errorf("Expected %+v got %+v\n", test.Expect, result)
+			}
+		})
+	}
+}
