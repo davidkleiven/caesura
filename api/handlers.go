e823d30d2c12814a3fa89377222ecba5a404d7fb
David Kleiven <davidkleiven446@gmail.com>
2025-07-21 17:49:57 +0200

    feat(web): allow filter to be pre-populated with values

--- api/handlers.go
+++ api/handlers.go
@@ -20,7 +20,7 @@ type HandlerFunc func(http.ResponseWriter, *http.Request)
 func RootHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	w.Write(web.Index())
+	w.Write(web.Index(&web.ScoreMetaData{}))
 }
 
 func InstrumentSearchHandler(w http.ResponseWriter, r *http.Request) {
