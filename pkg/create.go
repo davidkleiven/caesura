33fc187be77927d97b85148ae9e10c925afc28ae
David Kleiven <davidkleiven446@gmail.com>
2025-08-16 07:53:09 +0200

    maint: remove unused function

--- pkg/create.go
+++ pkg/create.go
@@ -1,14 +1,12 @@
 package pkg
 
 import (
-	"archive/zip"
 	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
 	"strconv"
 
-	"github.com/davidkleiven/caesura/utils"
 	"github.com/pdfcpu/pdfcpu/pkg/api"
 	"github.com/pdfcpu/pdfcpu/pkg/pdfcpu/primitives"
 )
@@ -44,19 +42,4 @@ func CreateNPagePdf(w io.Writer, n int) error {
 		return err
 	}
 	return api.Create(nil, bytes.NewBuffer(data), w, nil)
-}
-
-// MustCreateResource creates a new resource. It panics on any error and
-// is intended for setting up resources for testing purposes
-func MustCreateResource(numFiles int) []byte {
-	var buffer bytes.Buffer
-	zipWriter := zip.NewWriter(&buffer)
-	for i := range numFiles {
-		pageWriter := utils.Must(zipWriter.Create(fmt.Sprintf("Part%d.pdf", i)))
-		PanicOnErr(CreateNPagePdf(pageWriter, 2))
-
-	}
-	PanicOnErr(zipWriter.Close())
-
-	return buffer.Bytes()
 }
