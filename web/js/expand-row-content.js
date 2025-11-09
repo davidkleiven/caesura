b6de4872e3373a398594589235816aff57c3f389
David Kleiven <davidkleiven446@gmail.com>
2025-11-09 19:15:37 +0100

    style: run prettier on the javascript

--- web/js/expand-row-content.js
+++ web/js/expand-row-content.js
@@ -1,16 +1,16 @@
 // htmx-expand-toggle.js
 
 async function toggleRowContent(id) {
-    const row = document.getElementById('expand-' + id);
-    if (!row.classList.contains('hidden')) {
-        row.classList.add('hidden');
-    } else {
-        const resp = await fetch(`/resources/${id}/content`)
-        if (!resp.ok) {
-            console.log("An error occured during fetch")
-        }
-
-        row.innerHTML = await resp.text()
-        row.classList.remove('hidden')
+  const row = document.getElementById("expand-" + id);
+  if (!row.classList.contains("hidden")) {
+    row.classList.add("hidden");
+  } else {
+    const resp = await fetch(`/resources/${id}/content`);
+    if (!resp.ok) {
+      console.log("An error occured during fetch");
     }
+
+    row.innerHTML = await resp.text();
+    row.classList.remove("hidden");
   }
+}
